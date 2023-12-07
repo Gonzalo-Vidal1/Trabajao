99e5d9b790d8ea47ca956e190b940a6ffbe29838
Gonzalo-Vidal1 <gonzalovidal13@gmail.com>
2023-12-07 20:05:51 -0300

    actualizacion

---
+++ js/filtro.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let currentSlide = 0;
+    const slides = document.querySelectorAll('.slide');
+    const totalSlides = slides.length;
+
+    function showSlide(index) {
+        slides.forEach((slide, i) => {
+            slide.style.display = (i === index) ? 'block' : 'none';
+        });
+    }
+
+    function nextSlide() {
+        currentSlide = (currentSlide + 1) % totalSlides;
+        showSlide(currentSlide);
+    }
+
+    setInterval(nextSlide, 3000); // Cambia de slide cada 3 segundos
+
+    // Mostrar el primer slide al cargar la página
+    showSlide(currentSlide);
+});
